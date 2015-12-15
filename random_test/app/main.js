f539d2a8b537cf9704de032840ea964f07a65d86
Jennifer Jacobs <jacobsj@media.mit.edu>
2015-12-15 17:22:53 -0500

    xy transformations

--- random_test/app/main.js
+++ random_test/app/main.js
@@ -87,13 +87,13 @@ define(['paper', 'app/SStroke', 'app/Line', 'app/Path', 'app/Stroke', 'app/Signa
 
 			//var s2 = sstroke.distort(currentPath.spine);
 			var pathStroke = new SStroke(currentPath.spine);
-			generateGuides(pathStroke.inflectionPoints);
+			var guides = generateGuides(pathStroke.inflectionPoints);
 			var group = new paper.Group();
 			group.addChild(pathStroke.proto.clone());
-			/*for(var i=0;i<30;i++){
-				pathStroke = jitterPath(pathStroke, pathStroke.proto);
+			for (var i = 0; i < 30; i++) {
+				pathStroke = jitterPath(guides, pathStroke, pathStroke.proto);
 				group.insertChild(0,pathStroke.proto.clone());
-			}*/
+			}
 			pathStroke.proto.strokeColor = {
 				hue: 150,
 				saturation: 0.72,
@@ -115,38 +115,40 @@ define(['paper', 'app/SStroke', 'app/Line', 'app/Path', 'app/Stroke', 'app/Signa
 	};
 
 	function generateGuides(inflectionPoints) {
+		var guides = [];
 		for (var i = 0; i < inflectionPoints.length; i++) {
 			var p = new paper.Point(inflectionPoints[i].x, inflectionPoints[i].y);
 			var guide = new paper.Path.Line(p, new paper.Point(p.x, p.y + 602));
 			guide.translate(0, -p.y);
-			guide.strokeColor = {
+			/*guide.strokeColor = {
 				hue: 206,
 				saturation: 0.72,
 				brightness: 1
-			};
-guide.strokeWidth =2;
+			};*/
+			guide.strokeWidth = 2;
+			var rotation = guide.clone();
+
+			rotation.rotate(10, p);
+			var dist = rotation.firstSegment.point.y;
+			console.log('dist', dist);
+			//rotation.strokeColor = 'red';
+			var rV = rotation.firstSegment.point.subtract(rotation.lastSegment.point);
+			//rV.normalize();
+			console.log('rV', rV, rV.angle, rV.length);
+			guide.data.dir = inflectionPoints[i].dir;
+			guides.push({
+				guide: guide,
+				rot: rotation
+			});
 		}
-		
+		return guides;
 	}
 
-	function jitterPath(stroke, path) {
+	function jitterPath(guides, stroke, path) {
 		//path.selected = true;
 		var utils = new Utils();
-		var addInflection = Math.random() * 1;
-		var removeInflection = Math.random() * 1;
-		if (addInflection < 0.5) {
-			//var r_offset = Math.random()*stroke.spine.length;
-			//var p = stroke.spine.getPointAt(r_offset);
-			//console.log("added inflection point",p);
-			//stroke.inflectionPoints.push(p);
-		}
-
-
-		if (removeInflection < 0.25) {
-			//var toRemove =Math.round(Math.random()*stroke.inflectionPoints.length-1);
-			//inflectionPoints.splice(toRemove,1);
-			//console.log('removed at ',toRemove);
-		}
+		//var addInflection = Math.random() * 1;
+		//	var removeInflection = Math.random() * 1;
 
 		//var alteredPoints = [];
 		var path2 = new paper.Path();
@@ -160,46 +162,68 @@ guide.strokeWidth =2;
 			pathClone.opacity = 0;
 		}
 
-		for (var i = 0; i < stroke.inflectionPoints.length; i++) {
+		for (var i = 0; i < guides.length; i++) {
 
-
+			var guide = guides[i].guide;
+			var rot = guides[i].rot;
 			//console.log(i,shift1,shift2);
-			var s = stroke.inflectionPoints[i];
-			var i_point = new paper.Point(s.x, s.y);
-			var i_dist = path.getOffsetOf(i_point);
-			for (var j = 0; j < path.segments.length; j++) {
-				var dist = path.segments[j].location.offset;
-				var dist_points = Math.abs(i_dist - dist);
-
-				var dist_to_inflect = utils.map_range(dist_points, 0, path.length, 1, 0);
-				var decay = Math.pow(Math.E, (25 * (dist_to_inflect - 1)));
-				var s2 = pathClone.segments[j];
-				var shift;
+			//var s = stroke.inflectionPoints[i];
+			var dir = guide.data.dir;
+			var intersections = guide.getIntersections(path);
+			if (intersections.length > 0) {
+				var i_point = intersections[0].point;
+				var y1 = rot.firstSegment.point.y;
+				var y0 = rot.lastSegment.point.y;
+				var x1 = rot.firstSegment.point.x;
+				var x0 = rot.lastSegment.point.x;
+				var r_point = (i_point.y - y0) / ((y1 - y0) / (x1 - x0)) + x0;
+				var r_dist = r_point - i_point.x;
+				//console.log('i_point', i_point, 's',r_dist);
+				var i_dist = path.getOffsetOf(i_point);
 				var p = new paper.Path.Circle(i_point, 3);
-				if (s.dir == 1) {
-					shift = -(Math.random() * (100) - 50) * decay;
-					p.fillColor = 'red';
+				var p2 = new paper.Path.Circle(new paper.Point(r_point, i_point.y), 3);
+				//p2.fillColor = 'orange';
+				if (dir == 1) {
+
+					//p.fillColor = 'red';
 
 				} else {
-					shift = (Math.random() * (100) - 50) * decay;
-					p.fillColor = 'green';
+					//p.fillColor = 'green';
 
 
 				}
-				//console.log("inflection=",i, "segment=",j,"dist=",dist_to_inflect, "decay=",decay,"shift=",shift);
-				s2.point.y += shift;
-				//s2.point.x = s2.point.x + shift1;
+
+				for (var j = 0; j < path.segments.length; j++) {
+					var dist = path.segments[j].location.offset;
+					var dist_points = Math.abs(i_dist - dist);
+
+					var dist_to_inflect = utils.map_range(dist_points, 0, path.length, 1, 0);
+					var ydecay = Math.pow(Math.E, (25 * (dist_to_inflect - 1)));
+					var xdecay = Math.pow(Math.E, (10 * (dist_to_inflect - 1)));
+					var s2 = pathClone.segments[j];
+					var shift;
+					if (dir == 1) {
+						shift = -10 * ydecay;
+
+					} else {
+						shift = 10 * ydecay;
+
+					}
+					console.log("inflection=",i, "segment=",j,"dist=",dist_to_inflect, "decay=",xdecay,"shift=",r_dist*xdecay);
+					s2.point.y += shift;
+					s2.point.x += r_dist* xdecay;
+				}
+
+
+				path2.add(i_point);
 			}
-
-
-			path2.add(i_point);
-
 
 
 		}
 		pathClone.translate(0, -5);
+		pathClone.smooth();
 		return new SStroke(pathClone);
-		//path2.smooth();
+		
 
 
 	}
